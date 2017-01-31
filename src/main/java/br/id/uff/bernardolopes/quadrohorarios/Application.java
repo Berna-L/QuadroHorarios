cdf401292a0ba1b6e68797940f9f778f4cf429a2
Bernardo Lopes <bernardolopes@id.uff.br>
2017-01-31 09:38:47 -0200

    CLEANUP por Bernardo Lopes:
    Criação de classe de execução.

---
+++ src/main/java/br/id/uff/bernardolopes/quadrohorarios/Application.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.id.uff.bernardolopes.quadrohorarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ *
+ * @author bernardolopes at id.uff.br
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+}
